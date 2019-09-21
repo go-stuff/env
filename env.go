7d27a2aa70fe36990bc585ab89182ef2f3361761
Stephen Michaelis <Stephen.Michaelis@hotmail.com>
2019-09-21 09:02:17 -0400

    added zap logger

--- env.go
+++ env.go
@@ -2,56 +2,87 @@ package env
 
 import (
 	"bufio"
-	"fmt"
-	"log"
 	"os"
 	"regexp"
+
+	"go.uber.org/zap"
 )
 
 // File parses a key, value pair file as environment variables.
 func File(path string) error {
 
-	// dose the file exist
-	_, err := os.Stat(path)
+	// initialize logging
+	logger, err := zap.NewProduction()
+	if err != nil {
+		return err
+	}
+	defer logger.Sync()
+
+	// if the files does not exist, assume it is using actual environment variables
+	_, err = os.Stat(path)
 	if os.IsNotExist(err) {
-		log.Printf("INFO > env.go > File(): %s does not exist\n", path)
-	} else {
-		// open .env
-		file, err := os.Open(path)
-		if err != nil {
-			return err
-		}
-		defer file.Close()
-
-		// read each line
-		scanner := bufio.NewScanner(file)
-		for scanner.Scan() {
-			if scanner.Text() != "" {
-				regex := regexp.MustCompile(`([a-zA-Z0-9-_]*)\s*=\s*"(.*)"`)
-				matches := regex.FindStringSubmatch(scanner.Text())
-				if len(matches) != 3 {
-					return fmt.Errorf("error in %s", path)
-				}
-
-				// []matches = [0]line, [1](group1), [2](group2)
-				err := os.Setenv(matches[1], matches[2])
-				if err != nil {
-					return err
-				}
-
-				// DO NOT PRINT ENVIRONMENT VARIABLES OUT, IT IS UNSAFE
-				// fmt.Printf("env:%s = %s\n", matches[1], os.Getenv(matches[1]))
-			}
-		}
-
-		// catch scanner errors
-		err = scanner.Err()
-		if err != nil {
-			return err
-		}
-
-		log.Printf("INFO > env.go > File(): %s loaded\n", path)
+		logger.Warn(
+			"file does not exist",
+			zap.String("path", path),
+			zap.String("err", err.Error()),
+		)
+		return nil
 	}
 
+	// open environment file (example: .env)
+	file, err := os.Open(path)
+	if err != nil {
+		logger.Error(
+			"unable to open file",
+			zap.String("err", err.Error()),
+		)
+		return err
+	}
+	defer file.Close()
+
+	// read each line
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		if scanner.Text() != "" {
+			regex := regexp.MustCompile(`([a-zA-Z0-9-_]*)\s*=\s*"(.*)"`)
+			matches := regex.FindStringSubmatch(scanner.Text())
+
+			if len(matches) != 3 {
+				logger.Error(
+					"unable to make a match",
+					zap.String("err", err.Error()),
+				)
+				return err
+			}
+
+			// []matches = [0]line, [1](group1), [2](group2)
+			err := os.Setenv(matches[1], matches[2])
+			if err != nil {
+				logger.Error(
+					"unable to set environment variable",
+					zap.String("err", err.Error()),
+				)
+				return err
+			}
+
+			// DO NOT PRINT ENVIRONMENT VARIABLES OUT, IT IS UNSAFE
+			// fmt.Printf("env:%s = %s\n", matches[1], os.Getenv(matches[1]))
+		}
+	}
+
+	// catch scanner errors
+	err = scanner.Err()
+	if err != nil {
+		logger.Error(
+			"errors with file",
+			zap.String("err", err.Error()),
+		)
+		return err
+	}
+
+	logger.Info(
+		"environment file was loaded",
+		zap.String("path", path),
+	)
 	return nil
 }
